29f09d5fad970c6396f61bbbece26084e4956372
JUDHI3134 <judhistirbehera241@gmail.com>
2024-12-20 18:52:38 +0530

    create hero section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
 
 export const AppcontextProvider = (props) =>{
 
+    const [searchFilter, setSearchFilter] = useState({
+        title:'',
+        location:''
+    })
+    const [isSearched, setIsSearched] = useState(false)
+
 
     const value = {
-
+        searchFilter, setSearchFilter,isSearched, setIsSearched,
     }
 
 
