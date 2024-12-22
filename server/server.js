92670cc070acf98477aab39a71555711cb710f30
JUDHI3134 <judhistirbehera241@gmail.com>
2024-12-22 15:02:08 +0530

    db connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
+import connectDB from "./config/db.js";
 
 
 //initialize express
 const app = express();
 
+//database connection
+await connectDB();
+
 
 //middelwares
 app.use(cors())
 app.use(express.json())
+
 
 //route
 app.use("/",(req,res) =>{
