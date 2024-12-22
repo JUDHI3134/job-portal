6b0c5406d31a16ccb16147ebd0698389b8c3c3ee
JUDHI3134 <judhistirbehera241@gmail.com>
2024-12-22 12:53:53 +0530

    create manage jobs page ui

--- client/src/pages/AddJobs.jsx
+++ client/src/pages/AddJobs.jsx
@@ -79,7 +79,7 @@ const AddJobs = () => {
         <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' onChange={e => setSalary(e.target.value)} value={salary} type="Number" placeholder='25000' />
       </div>
 
-      <button className='w-28 py-3 mt-4 text-white bg-emerald-500' type='submit'>ADD</button>
+      <button className='w-28 py-3 mt-4 text-white bg-emerald-500 rounded' type='submit'>ADD</button>
 
 
     </form>
