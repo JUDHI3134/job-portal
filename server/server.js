6b0fe5e9d10dd5cf954dd525077ecab139657a27
JUDHI3134 <judhistirbehera241@gmail.com>
2024-12-22 15:22:49 +0530

    add sentry for error debouging

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+import "./config/instrument.js"
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./config/db.js";
+import * as Sentry from "@sentry/node";
 
 
 //initialize express
@@ -17,11 +19,17 @@ app.use(express.json())
 
 
 //route
-app.use("/",(req,res) =>{
+app.get("/",(req,res) =>{
     res.send("Api Working")
 })
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  
 
+//port
 const PORT = process.env.PORT || 5000
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server listen port ${PORT}`)
