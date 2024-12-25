231424aeeae4d3b0f5137e3db526076f16414e97
JUDHI3134 <judhistirbehera241@gmail.com>
2024-12-25 11:22:51 +0530

    get user data in apply jobs page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,16 @@
 import { createContext, useEffect, useState } from "react";
-import { jobsData } from "../assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
 export const AppcontextProvider = (props) =>{
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const {user} = useUser()
+    const {getToken} =useAuth()
 
     const [searchFilter, setSearchFilter] = useState({
         title:'',
@@ -20,9 +23,23 @@ export const AppcontextProvider = (props) =>{
     const [companyToken, setCompanyToken] = useState(null)
     const [companyData, setCompanyData] = useState(null)
 
+    const [userData, setUserData] = useState(null)
+    const [userApplications, setUserApplications] = useState(null)
+
     //Functions to fetch jobs
     const fetchJobs = async () =>{
-        setJobs(jobsData);
+        try {
+            const {data} = await axios.get(backendUrl+"/api/jobs")
+
+            if(data.success){
+                setJobs(data.jobs)
+                console.log(data.jobs)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
 
     //function to fetch company data
@@ -34,6 +51,23 @@ export const AppcontextProvider = (props) =>{
             if(data.success){
                 setCompanyData(data.company)
                 console.log(data)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    //function to fetch user data
+    const fetchUserData = async () =>{
+        try {
+            const token = await getToken()
+
+            const {data} = await axios.get(backendUrl+"/api/users/user",{headers:{Authorization: `Bearer ${token}`}})
+
+            if (data.success) {
+                setUserData(data.user)
             }else{
                 toast.error(data.message)
             }
@@ -58,6 +92,10 @@ export const AppcontextProvider = (props) =>{
             fetchCompanyData()
         }
     },[companyToken])
+
+    useEffect(()=>{
+        fetchUserData()
+    },[user])
 
 
     const value = {
--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -8,6 +8,8 @@ import kconvert from "k-convert"
 import moment from "moment"
 import JobCard from '../components/JobCard';
 import Footer from '../components/Footer';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const ApplyJob = () => {
 
@@ -15,20 +17,24 @@ const ApplyJob = () => {
 
   const [jobData, setJobData] = useState(null)
 
-  const { jobs} = useContext(AppContext)
+  const { jobs, backendUrl} = useContext(AppContext)
 
   const fetchJobs = async () =>{
-      const data = jobs.filter(job => job._id === id)
-      if(data.length !== 0){
-        setJobData(data[0])
-        console.log(data[0])
+      try {
+        const {data} = await axios.get(backendUrl+`/api/jobs/${id}`)
+
+        if (data.success) {
+          setJobData(data.job)
+        }else{
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message)
       }
   }
 
-  useEffect(()=>{
-    if(jobs.length > 0){
+  useEffect(()=>{   
       fetchJobs();
-    }
   },[id,jobs])
 
   return jobData ?  (
